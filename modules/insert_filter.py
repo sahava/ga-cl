25154b60ef5b904bb1186eea0da31785efe5c918
Simo Ahava <simo.s.ahava@gmail.com>
2018-09-12 16:37:13 +0300

    Switched profile link to prompts from user

--- modules/insert_filter.py
+++ modules/insert_filter.py
@@ -2,16 +2,31 @@ import sys
 import auth
 import json
 import argparse
+import re
 
-def link_filters(service, created_filters, account_id, web_property_id, profile_id):
+def link_filters(service, created_filters, account_id):
+    ua_regex = re.compile(r'^UA-\d+-\d+$', re.IGNORECASE)
+    property_input = ''
+    while ua_regex.match(property_input) is None and property_input is not 'q':
+        property_input = raw_input('Please enter the Web Property ID (e.g. UA-%s-1) of the web property in which the links will be created (enter \'q\' to quit): ' % account_id)
+    if property_input is 'q':
+        sys.exit()
+    
+    pid_regex = re.compile(r'^\d+$')
+    profile_input = ''
+    while pid_regex.match(profile_input) is None and profile_input is not 'q':
+        profile_input = raw_input('Please enter the Profile ID of the Google Analytics profile that you want to link the filters to (enter \'q\' to quit): ')
+    if profile_input is 'q':
+        sys.exit()
+      
     first_rank = None;
     last_rank = 1;
     for filter in created_filters:
-        print("Linking filter %s to profile %s in web property %s" % (filter.get('name'), profile_id, web_property_id))
+        print('Linking filter %s to profile %s in web property %s' % (filter.get('name'), profile_input, property_input))
         created_link = service.management().profileFilterLinks().insert(
             accountId=account_id,
-            webPropertyId=web_property_id,
-            profileId=profile_id,
+            webPropertyId=property_input,
+            profileId=profile_input,
             body={
                 'filterRef': {
                     'id': filter.get('id')
@@ -21,7 +36,17 @@ def link_filters(service, created_filters, account_id, web_property_id, profile_
         if first_rank is None:
             first_rank = created_link.get('rank')
         last_rank = created_link.get('rank')
-    print('Successfully created %s filters in profile %s in positions %s to %s' % (len(created_filters), profile_id, first_rank, last_rank))
+    if len(created_filters) is 1:
+        print('Successfully created 1 filter in profile %s in position %s\n' % (profile_input, first_rank))
+    else:
+        print('Successfully created %s filters in profile %s in positions %s to %s\n' % (len(created_filters), profile_input, first_rank, last_rank))
+    
+    yes_no = False
+    while yes_no not in ['y', 'n']:
+        yes_no = raw_input('Add another profile link (y/n)? ')
+    if yes_no is 'n':
+        sys.exit()
+    link_filters(service, created_filters, account_id)        
           
 
 def create_filters(service, json_path, account_id):
@@ -29,43 +54,45 @@ def create_filters(service, json_path, account_id):
     with open(json_path) as myfile:
         myfile = json.load(myfile)
         for filter in myfile:
-            print("Creating filter %s in account %s" % (filter.get('name'), account_id))
+            print('Creating filter %s in account %s' % (filter.get('name'), account_id))
             created_filters.append(
                 service.management().filters().insert(
                     accountId=account_id,
                     body=filter
                 ).execute()
             )
-    print("Successfully created %s filters in account %s" % (len(created_filters), account_id))
+    print('Successfully created %s filters in account %s\n' % (len(created_filters), account_id))
     return created_filters
             
 
 def main(argv):
     scope = ['https://www.googleapis.com/auth/analytics.edit']
 
-    parser = argparse.ArgumentParser(description='Create and link filters to a Google Analytics account/profile. You can get the Account ID and the Profile ID from the URL when viewing a profile in the Google Analytics UI. The Account ID is the string after "a" and the Profile ID is the string after "p" in the URL\'s /a12345w12345p12345 part.')
+    parser = argparse.ArgumentParser(description='Create and link filters to a Google Analytics account/profile. You will need the Google Analytics Account ID, Web Property ID (UA-XXXXX-Y), and the Profile ID at hand. You can find the Account ID and Profile ID in the URL of the Google Analytics UI (when viewing any profile). Account ID is the numerical string after \'a\' and Profile ID is the numerical string after \'p\' in the URL\'s /aXXXXwXXXXpXXXX part.')
     
     required = parser.add_argument_group('required arguments')
     required.add_argument('-c', '--create', metavar='accountid', help='Create the filters in the given Google Analytics account ID', required=True, dest='accountid')
     required.add_argument('-j', '--jsonpath', help='The path to the JSON file', required=True, metavar='/path/to/file.json')
     
     parser.add_argument('path_to_client_secrets', help='Path to your client_secrets file')
-    parser.add_argument('-w', '--webpropertyid', metavar='UA-XXXXX-Y', help='Web property ID where the filters will be linked to a profile')
-    parser.add_argument('-p', '--profileid', metavar='profileid', help='Google Analytics profile ID to which you want to link the filters')
     
     args = parser.parse_args()
 
     account_id = args.accountid
     json_path = args.jsonpath
-    web_property_id = args.webpropertyid
-    profile_id = args.profileid
     client_secrets_path = args.path_to_client_secrets
 
     service = auth.auth('analytics', 'v3', scope, client_secrets_path)
     
     created_filters = create_filters(service, json_path, account_id)
-    if web_property_id is not None and profile_id is not None:
-        link_filters(service, created_filters, account_id, web_property_id, profile_id)
+    
+    yes_no = False
+    while yes_no not in ['y', 'n']:
+        yes_no = raw_input('Do you wish to link these filters to a Google Analytics profile (y/n)? ')
+    if yes_no is 'n':
+        sys.exit()
+    
+    link_filters(service, created_filters, account_id)
 
 
 if __name__ == '__main__':
